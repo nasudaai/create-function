45cd6699051c354cd4b23d0609ed68cd76d75d58
nasuda <nasuda908@gmail.com>
2023-08-28 07:37:00 +0900

    add first function

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const app = document.getElementById('app');
 console.log(app);
+
+const createElm = (elm) => {
+ const newElm = document.createElement(elm);
+ return newElm;
+}
+
+const h = createElm('h1');
+h.textContent = 'hello';
+app.appendChild(h);
+console.log(h);
